646ee4ab0b750a9fa5456786e337259959c76cfc
Artsiom Dziominski <artsiomdziominski@gmail.com>
2022-12-29 14:41:57 +0400

    CV Portfolio Artsiom Dziominski

---
+++ js/themes.js
@@ -0,0 +1,22 @@
+const THEME = 'theme';
+const THEME_LIGHT = 'theme-light';
+const THEME_DARK = 'theme-dark';
+const THEME_LIGHT_IMAGE = 'static/img/light_mode.svg';
+const THEME_DARK_IMAGE = 'static/img/dark_mode.svg';
+
+const theme = document.getElementById(THEME);
+const changeThemeButton = document.getElementById('change-theme');
+const changeThemeButtonImage = document.getElementById('change-theme-img');
+
+theme.className = localStorage.getItem(THEME) || THEME_LIGHT;
+changeThemes();
+
+changeThemeButton.addEventListener('click', () => {
+    theme.className = theme.className === THEME_DARK ? THEME_LIGHT : THEME_DARK;
+    changeThemes();
+    localStorage.setItem(THEME, theme.className)
+});
+
+function changeThemes() {
+    changeThemeButtonImage.src = theme.className === THEME_DARK ? THEME_LIGHT_IMAGE : THEME_DARK_IMAGE;
+}
